996e068ae29338d814d65fe3874ada4beb6f7733
Morongwa21 <angelmalatji5@gmail.com>
2023-08-19 20:26:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mysql = require('mysql');
-const express = require('express');
-const mysql = require('mysql');
+
 const app = express();
 const connection = mysql.createConnection({
   host: "localhost",         
@@ -11,28 +10,34 @@ const connection = mysql.createConnection({
 });
 
 // Connecting to MySQL
-db.connect(err => {
+connection.connect((err) => {
   if (err) {
     console.error('MySQL connection error:', err);
-  } else {
+  }
+   else {
     console.log('Connected to MySQL');
   }
 });
 
 
-app.get('/', (req, res)=> {
+app.get('/', (req, res) => {
 	return res.json("from server");
 })
 app.get('/citizen', (req, res) => {
 	const query="select from citizen";
-	db.query(sql, (err, data)=>{
+	connection.query(sql, (err, data) => {
 		if (err) {
 			return res.json(err);
 		}
 	return res.json(data);
 });
 });
+//close
+process.on('SIGINT', () => {
+  connection.end();
+  process.exit();
+});
 const port = process.env.PORT || 8080;
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
 	console.log("Listening on port", PORT);
 });
