0959410fd0d84dde46c98863bcb4584d6aea091e
Morongwa21 <angelmalatji5@gmail.com>
2023-08-21 14:58:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ process.on('SIGINT', () => {
   connection.end();
   process.exit();
 });
-const port = process.env.PORT || 8080;
+const PORT= process.env.PORT || 8000;
 app.listen(PORT, () => {
 	console.log("Listening on port", PORT);
 });
