1045b9feb073ef3263062591a7642816d87755dc
Morongwa21 <angelmalatji5@gmail.com>
2023-08-28 12:24:54 +0200

    Update citizen.js

---
+++ Modules/citizen.js
@@ -0,0 +1,31 @@
+// model Citizen.js
+
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/q_phela');
+
+const Citizen = sequelize.define('Citizen', {
+  citizen_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true, 
+  },
+  Citizen_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  cellphone: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = Citizen;
