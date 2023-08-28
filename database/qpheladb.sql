3ef1e81edcea356edd55fa2af3e7527039f42bf0
IamMohale <iammohalewilson@gmail.com>
2023-08-28 12:05:55 +0200

    database

--- database/qpheladb.sql
+++ database/qpheladb.sql
@@ -101,8 +101,7 @@ CREATE TABLE `safetytip` (
   `tip_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
-  `content` text NOT NULL,
-  `context` text NOT NULL 
+  `content` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
