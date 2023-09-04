7a5052c48856ed40daca92f95ff44b6a32469395
IamMohale <iammohalewilson@gmail.com>
2023-09-04 08:40:43 +0200

    receivers

--- controllers/alertControllers.js
+++ controllers/alertControllers.js
@@ -1,4 +1,23 @@
 const fsp = require('fs').promises;
 const fs = require('fs');
-exports.postAnalytics = async(req, res, next) => {}
-reportAnalytics.push({...req.body, createdAt: new Date()});
+exports.postAnalytics = async(req, res, next) => {
+    const{ip, coordinates} = req.body;
+    const validator = await schema.validateAsynch(req.body);
+    let reportAnalytics = [];
+    if (fs.existsSync(`${__dirname}/storeAnalytics.json`)) {
+        const reportFile = await fsp.readFile(`${__dirname}/storeAnalytics.json`, 'utf-8')
+        reportAnalytics = JSON.parse(reportFile)
+    } else {
+        return ('File does not exist');
+    }
+    reportAnalytics.push({...req.body, createdAt: new Date()})
+    await fsp.writeFile('${__dirname}/storeAnalytics.json', JSON.stringify(reportAnalytics))
+    res.status(201).json({
+        status: 'succesful',
+        data: {
+            message: 'Information succesfully taken'
+        }
+    })
+    
+    
+};
