1e5251b66e97bf1602a3c606d4d745682f1656b5
Morongwa21 <angelmalatji5@gmail.com>
2023-09-07 16:26:51 +0200

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,25 +1,35 @@
-
+const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
-//const config = require('./config/config'); // Your database and secret key configuration
+const cors=require('cors');
+//const db= require('./config/database'); // Your database and secret key configuration
+
+// MySQL Connection Configuration
+const mysql = require('mysql');
+const db = mysql.createConnection({
+  host: "localhost",         // Replace with the actual host if hosted remotely
+  user: "root",
+  password: "",
+  database: "q_pheladb"
+});
 
 const app = express();
-
+//cccc
 app.use(bodyParser.json());
-
+app.use(cors());
 app.post('/register', (req, res) => {
-    const { username, password } = req.body;
-    if (!username || !password) {
-        return res.status(400).json({ message: 'Username and password are required' });
-      }
+    const { citizen_name, email, password,contact_number } = req.body;
+    if (!citizen_name || !email || !password || !contact_number) {
+        return res.status(400).json({ message: 'email and password are required' });
+      } /*
       bcrypt.hash(password, 10, (err, hashedPassword) => {
         if (err) {
           console.error(err);
           return res.status(500).json({ message: 'Registration failed' });
-        }
-        const sql = 'INSERT INTO users (username, password) VALUES (?, ?)';
-        db.query(sql, [username, hashedPassword], (err, result) => {
+        } */
+        const sql = 'INSERT INTO citizen (citizen_name, email, password, contact_number) VALUES (?, ?, ?, ?)';
+        db.query(sql, [citizen_name, email, password, contact_number], (err, result) => {
           if (err) {
             console.error(err);
             return res.status(500).json({ message: 'Registration failed' });
@@ -28,7 +38,7 @@ app.post('/register', (req, res) => {
           res.status(201).json({ message: 'Registration successful' });
         });
       });
-    });
+    //});
 
     app.post('/login', (req, res) => {
         const { username, password } = req.body;
@@ -41,27 +51,27 @@ app.post('/register', (req, res) => {
             return res.status(500).json({ message: 'Internal server error' });
           }
           if (results.length === 0) {
+            console.log('No user found with email:', email);
             return res.status(401).json({ message: 'Invalid credentials' });
           }
       
-          const user = results[0];
-          bcrypt.compare(password, user.password, (err, isPasswordValid) => {
-            if (err || !isPasswordValid) {
+          const citizen = results[0];
+
+          //bcrypt.compare(password, user.password, (err, isPasswordValid) => {
+            if (req.body.password !== citizen.password) {
+              console.log('Password mismatch for citizen:', citizen.citizen_name);
               return res.status(401).json({ message: 'Invalid credentials' });
             }
-      
-            // Generate a JWT token
-            const token = jwt.sign({ userId: user.id }, config.secretKey, { expiresIn: '1h' });
-      
-            res.status(200).json({ token });
+        
+         //   const token = jwt.sign({ citizen_id: citizen.id }, config.secretKey, { expiresIn: '1h' });
+        
+            res.status(200).json({ results });
           });
         });
-      });
-      const PORT = process.env.PORT || 3000;
+      const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-});      
-
+}); 
 /* Profile route i've authenticated
 router.get('/profile', authenticate, citizenController.getCitizenProfile);
 
