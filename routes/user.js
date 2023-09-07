97a7609d60706f4c545d806a46a8a15ebc865fae
Morongwa21 <angelmalatji5@gmail.com>
2023-09-07 06:25:31 +0200

    .

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,68 @@
 
 const router = express.Router();
-const citizenController = require('../controllers/citizenController');
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
+//const config = require('./config/config'); // Your database and secret key configuration
 
-router.post('/register', citizenController.register);
-router.post('/login', citizenController.login);
+const app = express();
 
-// Profile route i've authenticated
+app.use(bodyParser.json());
+
+app.post('/register', (req, res) => {
+    const { username, password } = req.body;
+    if (!username || !password) {
+        return res.status(400).json({ message: 'Username and password are required' });
+      }
+      bcrypt.hash(password, 10, (err, hashedPassword) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({ message: 'Registration failed' });
+        }
+        const sql = 'INSERT INTO users (username, password) VALUES (?, ?)';
+        db.query(sql, [username, hashedPassword], (err, result) => {
+          if (err) {
+            console.error(err);
+            return res.status(500).json({ message: 'Registration failed' });
+          }
+    
+          res.status(201).json({ message: 'Registration successful' });
+        });
+      });
+    });
+
+    app.post('/login', (req, res) => {
+        const { username, password } = req.body;
+      
+        // Retrieve the user from the MySQL database
+        const sql = 'SELECT * FROM users WHERE username = ?';
+        db.query(sql, [username], (err, results) => {
+          if (err) {
+            console.error(err);
+            return res.status(500).json({ message: 'Internal server error' });
+          }
+          if (results.length === 0) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+          }
+      
+          const user = results[0];
+          bcrypt.compare(password, user.password, (err, isPasswordValid) => {
+            if (err || !isPasswordValid) {
+              return res.status(401).json({ message: 'Invalid credentials' });
+            }
+      
+            // Generate a JWT token
+            const token = jwt.sign({ userId: user.id }, config.secretKey, { expiresIn: '1h' });
+      
+            res.status(200).json({ token });
+          });
+        });
+      });
+      const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});      
+
+/* Profile route i've authenticated
 router.get('/profile', authenticate, citizenController.getCitizenProfile);
 
 // Update Profile route i've authenticated
@@ -14,4 +71,4 @@ router.put('/profile', authenticate, citizenController.updateCitizenProfile);
 // Deletion of Account 
 router.delete('/:citizen_id', authenticate, citizenController.deleteCitizenAccount);
 
-module.exports = router;
+module.exports = router;*/
