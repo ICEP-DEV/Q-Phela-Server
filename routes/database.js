abc5efa6e3f9309477cdd18a643d47332d643e73
Morongwa21 <angelmalatji5@gmail.com>
2023-09-07 06:10:31 +0200

    m

--- routes/database.js
+++ routes/database.js
@@ -1,11 +1,11 @@
-const mysql = require('mysql2');
+const mysql = require('mysql');
 
 // MySQL Connection Configuration
 const db = mysql.createConnection({
   host: "localhost",         // Replace with the actual host if hosted remotely
   user: "root",
   password: "",
-  database: "q-phela database"
+  database: "q_pheladb"
 });
 
 // Connecting to MySQL
