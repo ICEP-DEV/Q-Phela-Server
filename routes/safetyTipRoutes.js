5e8801227b099f40c40fc438c934df2049dd755d
Morongwa21 <angelmalatji5@gmail.com>
2023-09-21 18:33:59 +0200

    Update safetyTipRoutes.js

--- routes/safetyTipRoutes.js
+++ routes/safetyTipRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-
 const mysql = require('mysql');
+
 const db = mysql.createConnection({
   host: "localhost",        
   user: "root",
@@ -44,16 +44,18 @@ app.get('/safety_tip/:tip_id', (req, res) => {
         res.status(200).json(results[0]);
     });
 });
-// Works add location one                            
+
+//add safety tip                           
 app.post('/safety_tip', (req, res) => {
-    const {  tip_description, citizen_id} = req.body; //location_id } = req.body;
-    if (!tip_description || !citizen_id){// || !location_id) {
+    const {  tip_description} = req.body; 
+    const citizen_id = req.user.citizen_id;
+    if (!tip_description || !citizen_id){
         return res.status(400).json({ message: 'Tip description,  citizen_id and location_id are required' });
     }
+    const sql = 'INSERT INTO safety_tips (tip_description, citizen_id) VALUES (?, ?)';
+    db.query(sql, [tip_description, citizen_id ], (err, result) => {
 
-    const sql = 'INSERT INTO safety_tips (tip_description, citizen_id) VALUES (?, ?)';// location_id) VALUES (?, ?, ?)';
-    db.query(sql, [tip_description,citizen_id ], (err, result) => {
-        // location_id], (err, result) => {
+ 
         if (err) {
             console.error(err);
             return res.status(500).json({ message: 'Failed to add safety tip' });
@@ -63,14 +65,14 @@ app.post('/safety_tip', (req, res) => {
             tip_description,
             citizen_id,
             date: new Date(),
-             //location_id,
+             
         };
 
         res.status(201).json({ message: 'Safety tip added successfully'});
     })
 
 });
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3006;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
